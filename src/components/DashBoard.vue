f75930d71ba84d67fec3e50ee852afaab342dd46
Seth250 <ayowaleakintayo@gmail.com>
2021-01-13 23:51:53 +0100

    styled pageview component

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -4,11 +4,11 @@
 			<p class="hello">Hello, <span>Emerald</span></p>
 			<p class="welcome">Welcome to your dashboard, kindly sort through the user base</p>
 		</div>
-		<div class="search">
-			<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill-rule="evenodd" clip-rule="evenodd" class="search__icon">
+		<div class="dashboard__search search">
+			<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill-rule="evenodd" clip-rule="evenodd" class="dashboard__search-icon">
 				<path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"/>
 			</svg>
-			<input type="text" placeholder="Find a user" class="search__input">
+			<input type="search" placeholder="Find a user" class="dashboard__search-input search-input">
 		</div>
 		<div class="user-select">
 			<p>Show Users</p>
--- src/components/PageView.vue
+++ src/components/PageView.vue
@@ -3,25 +3,28 @@
 		<div class="main__head">
 			<h1 id="title">{{ title }}</h1>
 			<p>Filter by</p>
-			<div>
-				<input type="search" placeholder="Find in list">
-			</div>
-			<div>
-				<select v-model="country">
-					<option value="" disabled>Country</option>
-					<option v-for="country in countries" :key="country" :value="country">{{ country }}</option>
-				</select>
-			</div>
-			<div class="">
-				<div class="slider" :class="sliderStyle">
-					<input
-						type="checkbox"
-						class="slider__box"
-						:checked="showCountries"
-						@click="updateCountriesChoice(!showCountries)"
-					>
+			<div class="main__facet">
+				<div class="main__search search">
+					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill-rule="evenodd" clip-rule="evenodd" class="main__search-icon">
+						<path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"/>
+					</svg>
+					<input type="search" class="main__search-input search-input" placeholder="Find in list">
 				</div>
-				Show Country
+				<select v-model="country" class="main__select">
+					<option class="main__option" value="" disabled>Country</option>
+					<option class="main__option" v-for="country in countries" :key="country" :value="country">{{ country }}</option>
+				</select>
+				<div class="main__toggle">
+					<div class="slider" :class="sliderStyle">
+						<input
+							type="checkbox"
+							class="slider__box"
+							:checked="showCountries"
+							@click="updateCountriesChoice(!showCountries)"
+						>
+					</div>
+					<p>Show Country</p>
+				</div>
 			</div>
 		</div>
 		<router-view />
